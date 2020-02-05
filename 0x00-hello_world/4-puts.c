c775ce1b9742602bd7c0536acfc5c275c09707b6
Eddyszh <eddyszh@gmail.com>
2020-02-05 20:34:53 +0000

    Learn low-programming with c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Puts: Message
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
