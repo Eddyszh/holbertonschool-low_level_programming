a59ebe8edcbe4aa9bf5be9bec53986dd7815bd9d
Eddyszh <eddyszh@gmail.com>
2020-03-05 18:25:21 +0000

    Fix task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,7 +10,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int *b, i;
 
-	b = malloc(size * nmemb + 1);
+	b = malloc(size * nmemb);
 	if (b == NULL)
 		return (NULL);
 	if (nmemb == 0 || size == 0)
