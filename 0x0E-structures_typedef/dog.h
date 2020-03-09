d0e1df0cee97d1cf1b95ed404f2375f77f0f8614
Eddyszh <eddyszh@gmail.com>
2020-03-09 22:05:26 +0000

    Fix tasks 0-1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef dog_H
+#define dog_H
 /**
  * struct dog - contains the features of dog
  * @name: pointer to char
@@ -8,8 +10,14 @@
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif /* dog_H */
