48c03818aa1cc330456cb1fcee8c69cf32a63102
Eddyszh <eddyszh@gmail.com>
2020-03-11 18:00:21 +0000

    Fix task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include <stddef.h>
-#include <stdlib.h>
 #include "function_pointers.h"
 /**
  * print_name - prints a name
@@ -8,7 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != '\0' || !f)
+	if (name != '\0' || f != name)
 		return;
 	(f)(name);
 }
