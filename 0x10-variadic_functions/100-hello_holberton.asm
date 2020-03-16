c78dba0c926e35b44deef94e4d491845ba6e7827
Eddyszh <eddyszh@gmail.com>
2020-03-16 04:18:10 +0000

    Do advanced task

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,13 @@
+section .data
+    text db "Hello, Holberton",10
+section .text
+    global main
+main:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, text
+    mov rdx, 17
+    syscall
+    mov rax, 68
+    mov rdi, 0
+    syscall
