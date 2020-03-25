47544d3c3bbe2c4bf763ce9200be6c1b2eb0e504
Eddyszh <eddyszh@gmail.com>
2020-03-25 05:13:19 +0000

    Fix some tasks

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	if (index == 0)
+	if (index == 0 && *head)
 	{
 		*head = (*head)->next;
 		free(temp);
@@ -25,8 +25,12 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		old = old->next;
 		index--;
 	}
-	temp = old->next;
-	old->next = temp->next;
-	free(temp);
-	return (1);
+	if (old)
+	{
+		temp = old->next;
+		old->next = temp->next;
+		free(temp);
+		return (1);
+	}
+	return (-1);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,9 +9,8 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *temp = *head;
-	listint_t *new;
+	listint_t *new = malloc(sizeof(listint_t));
 
-	new = malloc(sizeof(listint_t));
 	if (new == NULL || *head == NULL)
 		return (NULL);
 	if (idx == 0)
