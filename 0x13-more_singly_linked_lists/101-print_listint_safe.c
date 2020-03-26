dadff50caeff730e4ad7c935f7250254fb1942a1
Eddyszh <eddyszh@gmail.com>
2020-03-26 00:56:41 +0000

    Fix advanced task 101

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,8 +8,9 @@ size_t print_listint_safe(const listint_t *head)
 {
 	int i;
 
-	for (i = 0; head; i++)
+	for (i = 0; head;)
 	{
+		i++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (head - head->next > 0)
 		{
