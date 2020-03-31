8b9677ad98a2e38f58da24624a8f2a0b1b636304
Eddyszh <eddyszh@gmail.com>
2020-03-31 22:51:38 +0000

    Learn bout C file I/O, task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file created
+ * @text_content: content of the te created file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd;
+	char *end = text_content;
+
+	while (*end++)
+	{};
+	i = end - text_content - 1;
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT | O_EXCL, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		write(fd, text_content, 0);
+	else
+		write(fd, text_content, i);
+	return (1);
+}
