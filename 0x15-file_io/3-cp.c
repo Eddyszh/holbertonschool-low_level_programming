0aadd2a519028da5c1e09b984d53f8eed3152ea5
Eddyszh <eddyszh@gmail.com>
2020-04-01 04:33:36 +0000

    Fix task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,8 +11,7 @@ int main(int ac, char *av[])
 	int file_from, file_to, r = 1024, w;
 
 	if (ac != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	file_from = open(av[1], O_RDONLY);
 	if (file_from == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]),
@@ -32,11 +31,11 @@ int main(int ac, char *av[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]),
 			exit(99);
 	}
-	if (close(file_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from),
-		exit(100);
-	if (close(file_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
-		exit(100);
+	w = close(file_from);
+	if (w == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100);
+	w = close(file_to);
+	if (w == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100);
 	return (0);
 }
