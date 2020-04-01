932281c2f87b434cfc4cf3210ad41c15443ce8c9
Eddyszh <eddyszh@gmail.com>
2020-04-01 00:45:12 +0000

    Learn about c file I/O, task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file
+ * @text_content: string to add at the end of the file
+ * Return: 1 on success, -1 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int i, fd, w;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+	while (text_content[i])
+		i++;
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
