5eccc0b393b84955c03116673bdf2ecd7d3e7da3
Eddyszh <eddyszh@gmail.com>
2020-04-01 00:01:23 +0000

    Fix task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, fd;
+	int i, fd, w;
 	char *end = text_content;
 
 	while (*end++)
@@ -17,9 +17,13 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
-		write(fd, text_content, 0);
-	else
-		write(fd, text_content, i);
+	{
+		close(fd);
+		return (1);
+	}
+	w = write(fd, text_content, i);
+	if (w == -1)
+		return (-1);
 	text_content = '\0';
 	close(fd);
 	return (1);
