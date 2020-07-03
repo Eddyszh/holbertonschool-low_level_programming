7124b81abafd898c3c538790582e2ba14488c300
eddyszh <eddyszh@gmail.com>
2020-07-03 23:52:23 +0000

    Fix task 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,7 +7,7 @@ def island_perimeter(grid):
     """
     per = 0
     for x in range(len(grid)):
-        for y in range(len(grid)):
+        for y in range(len(grid[x])):
             if grid[x][y] == 1:
                 if x == 0 or grid[x - 1][y] == 0:
                     per += 1
